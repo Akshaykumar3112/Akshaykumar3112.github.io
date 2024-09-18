0fb93c6fd064ed9c86bb2aa1e7355186839b9cfc
Akshaykumar3112 <akshaykajavadara1998@gmail.com>
2024-09-18 10:52:07 +1000

    link update

--- src/Home.js
+++ src/Home.js
@@ -98,7 +98,7 @@ function Home() {
               }}
             >
                 <a
-            href="https://drive.google.com/file/d/1vLa60d9PI_Lv8MevSz3JCRAZJsY1zw6l/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1gNs8Fs2dMegj0QexqSn1JpUZyNpybVgV/view?usp=drive_link"
             
             style={{ textDecoration: "none", color: "inherit" }}
           >
